df2e65e134f16c0bf8486b7f9235add66aea2143
Antu <prince.anto97@gmai.com>
2021-02-18 23:37:30 +0600

    team section added

--- js/script2.js
+++ js/script2.js
@@ -16,3 +16,20 @@ function visibleForm() {
   var element = document.getElementById('join_form');
   element.classList.toggle('active_signup_form');
 }
+
+//===========
+// ============
+var coll = document.getElementsByClassName('collapsible');
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener('click', function () {
+    this.classList.toggle('active_mlb');
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    }
+  });
+}
