ed66227b000d8190904d4471eba41c244ee290e4
Md Sowrov <antukhadem17@gmail.com>
2022-08-01 18:05:04 +0600

    sorting enable in team page

--- js/script2.js
+++ js/script2.js
@@ -2,8 +2,8 @@
 window.onscroll = function () {
   myFunction();
 };
-var navbar = document.getElementById('navbar');
-var player_banner_fix = document.getElementById('player_bannr_sticky');
+var navbar = document.getElementById("navbar");
+var player_banner_fix = document.getElementById("player_bannr_sticky");
 
 // Get the offset position of the navbar
 // var sticky = navbar.offsetTop;
@@ -11,15 +11,15 @@ var player_banner_fix = document.getElementById('player_bannr_sticky');
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > 300) {
-    navbar.classList.add('sticky');
-    player_banner_fix.classList.add('player_info_sticky');
+    navbar.classList.add("sticky");
+    player_banner_fix.classList.add("player_info_sticky");
   } else {
-    navbar.classList.remove('sticky');
-    player_banner_fix.classList.remove('player_info_sticky');
+    navbar.classList.remove("sticky");
+    player_banner_fix.classList.remove("player_info_sticky");
   }
 }
 
-$('.testimonial_slider').slick({
+$(".testimonial_slider").slick({
   dots: false,
   infinite: true,
   autoplay: true,
@@ -59,7 +59,7 @@ $('.testimonial_slider').slick({
 });
 
 //
-$('.sponsor_slider').slick({
+$(".sponsor_slider").slick({
   dots: false,
   infinite: true,
   autoplay: true,
@@ -100,65 +100,65 @@ $(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
 
-$('.srch_btn').click(function () {
-  $('.search_box').toggleClass('active').focus;
-  $('.search_body').toggleClass('active').focus;
-  $('.close_search').toggleClass('active').focus;
-  $(this).toggleClass('animate');
-  $('.search_control').val('');
+$(".srch_btn").click(function () {
+  $(".search_box").toggleClass("active").focus;
+  $(".search_body").toggleClass("active").focus;
+  $(".close_search").toggleClass("active").focus;
+  $(this).toggleClass("animate");
+  $(".search_control").val("");
 });
 
-$('.close_search').click(function () {
-  $('.search_box').toggleClass('active').focus;
-  $('.search_body').toggleClass('active').focus;
-  $('.search_control').val('');
+$(".close_search").click(function () {
+  $(".search_box").toggleClass("active").focus;
+  $(".search_body").toggleClass("active").focus;
+  $(".search_control").val("");
 });
 
 // join form
 
 function visibleForm() {
-  var element = document.getElementById('join_form');
-  element.classList.toggle('active_signup_form');
+  var element = document.getElementById("join_form");
+  element.classList.toggle("active_signup_form");
 }
 
 //===========
 // ============
-var coll = document.getElementsByClassName('collapsible');
+var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener('click', function () {
-    this.classList.toggle('active_mlb');
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active_mlb");
     var content = this.nextElementSibling;
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
-      content.style.maxHeight = content.scrollHeight + 'px';
+      content.style.maxHeight = content.scrollHeight + "px";
     }
   });
 }
 
 // =========
-var btn = $('#button');
+var btn = $("#button");
 
 $(window).scroll(function () {
   if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
+    btn.addClass("show");
   } else {
-    btn.removeClass('show');
+    btn.removeClass("show");
   }
 });
 
-btn.on('click', function (e) {
+btn.on("click", function (e) {
   e.preventDefault();
-  $('html, body').animate({ scrollTop: 0 }, '300');
+  $("html, body").animate({ scrollTop: 0 }, "300");
 });
 
 //==========
 
 // Data table
 
-$('#p_batting').DataTable({
+$("#p_batting").DataTable({
   responsive: true,
   select: true,
   // dom: 'Bfrtip',
@@ -168,7 +168,7 @@ $('#p_batting').DataTable({
   paging: false,
 });
 
-$('#projection').DataTable({
+$("#projection").DataTable({
   responsive: true,
   select: true,
   // dom: 'Bfrtip',
@@ -178,11 +178,11 @@ $('#projection').DataTable({
   paging: false,
 });
 
-$('#pitching').DataTable({
+$("#pitching").DataTable({
   responsive: true,
   select: true,
   searching: false,
-  // ordering: false,
+  ordering: true,
 });
 
 // slider
